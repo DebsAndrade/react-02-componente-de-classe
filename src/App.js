6ad68ccc774f81155923685f9293f526157584ee
DeboraAndrade <100317147+DebsAndrade@users.noreply.github.com>
2022-07-13 12:31:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Desafio02 extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Olá Ster e Leandro, segue meu desafio de React 02, do componente de
+          classe.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Desafio02;
